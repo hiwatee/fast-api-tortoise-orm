a52171afc29695547a0437bd61dc0f864bcc551b
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2020-12-19 16:43:46 +0900

    feat: change endpoint

--- routers/users.py
+++ routers/users.py
@@ -21,17 +21,12 @@ async def register_user(form_data: UserIn):
     return await User.create(**form_data.dict(exclude_unset=True))
 
 
-@router.get("/me", tags=tags, response_model=User_Pydantic)
+@router.get("/current_user", tags=tags, response_model=User_Pydantic)
 async def get_user_data(user: User = Depends(get_current_user)):
     return user
 
 
-@router.patch("/me", tags=tags, response_model=User_Pydantic)
+@router.patch("/current_user", tags=tags, response_model=User_Pydantic)
 async def update_user(form_data: UserIn_Pydantic, user: User = Depends(get_current_user)):
     await user.update_from_dict(form_data.dict(exclude_unset=True)).save()
     return user
-
-
-@router.delete("/me", tags=tags, status_code=HTTP_204_NO_CONTENT)
-async def delete_user(user: User = Depends(get_current_user)):
-    await user.soft_delete()
