b20cf00b617055e97ed597275750007ff1581b89
hiwatee <m-hiwatashi@kakusin-g.co.jp>
2020-12-19 16:40:07 +0900

    feat: add swagger login

--- routers/token.py
+++ routers/token.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form
 from pydantic import BaseModel
 from models.user import User
 from services.auth import login_with_password, get_current_user
@@ -25,7 +25,6 @@ class TokenRefreshIn(BaseModel):
 
 @router.post("/token", tags=tags, response_model=GetTokenOut)
 async def get_token_with_password(form_data: GetTokenIn):
-    print(form_data)
     user = await login_with_password(form_data.email, form_data.password)
     return GetTokenOut(access_token=user.get_access_token(), refresh_token=user.refresh_token.hex)
 
@@ -34,4 +33,10 @@ async def get_token_with_password(form_data: GetTokenIn):
 async def token_refresh(form_data: TokenRefreshIn, user: User = Depends(get_current_user)):
     if form_data.refresh_token != user.refresh_token.hex:
         raise HTTP_404_NOT_FOUND
+    return GetTokenOut(access_token=user.get_access_token(), refresh_token=user.refresh_token.hex)
+
+
+@router.post("/login")
+async def login(username: str = Form(...), password: str = Form(...)):
+    user = await login_with_password(username, password)
     return GetTokenOut(access_token=user.get_access_token(), refresh_token=user.refresh_token.hex)
--- services/auth.py
+++ services/auth.py
@@ -11,7 +11,7 @@ from services.exceptions import HTTP_401_UNAUTHORIZED
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
